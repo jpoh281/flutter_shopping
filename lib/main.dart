25e223d55074c7c6a54acdac3a4534d8b9351a02
jpoh281 <hjp4869@naver.com>
2021-12-11 17:17:22 +0900

    💄 StatusBar 색 및 화면 틀 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_shopping/shopping_screen.dart';
 
 void main() {
@@ -10,12 +11,17 @@ class ShoppingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    // 안드로이드에서 StatusBar의 색과 안드로이드와 iOS 모두에서 StatusBar 아이콘 색상을
+    // 설정하기 위해 AnnotatedRegion을 사용함.
+    return const AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+        statusBarColor: Colors.white,
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: ShoppingScreen(),
       ),
-      home: const ShoppingScreen(),
     );
   }
 }
--- lib/shopping_screen.dart
+++ lib/shopping_screen.dart
@@ -5,6 +5,106 @@ class ShoppingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // iOS에서 StatusBar의 색상을 정해주기위해 설정
+    // TabBar 등을 사용하려면 Scaffold 혹은 Meterial로 감싸줘야함.
+    return Material(
+      color: Colors.white,
+      child: SafeArea(
+        child: DefaultTabController(
+          length: 3,
+          child: CustomScrollView(
+            slivers: [
+              const SliverAppBar(
+                pinned: false,
+                backgroundColor: Colors.white,
+                title: Text(
+                  "Flutter Shopping App",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              const SliverPersistentHeader(
+                  pinned: true, delegate: TabBarDelegate()),
+              SliverFillRemaining(
+                // 탭바 뷰 내부에는 스크롤이 되는 위젯이 들어옴.
+                hasScrollBody: true,
+                child: TabBarView(
+                  children: [
+                    Container(
+                      color: Colors.amber,
+                    ),
+                    Container(
+                      color: Colors.redAccent,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TabBarDelegate extends SliverPersistentHeaderDelegate {
+  const TabBarDelegate();
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.white,
+      child: TabBar(
+        tabs: [
+          Tab(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              color: Colors.white,
+              child: const Text(
+                "홈",
+              ),
+            ),
+          ),
+          Tab(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              color: Colors.white,
+              child: const Text(
+                "특가",
+              ),
+            ),
+          ),
+          Tab(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              color: Colors.white,
+              child: const Text(
+                "랭킹",
+              ),
+            ),
+          ),
+        ],
+        indicatorWeight: 2,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        unselectedLabelColor: Colors.grey,
+        labelColor: Colors.black,
+        indicatorColor: Colors.black,
+        indicatorSize: TabBarIndicatorSize.label,
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 48;
+
+  @override
+  double get minExtent => 48;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
